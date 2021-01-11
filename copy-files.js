fde1427c89ed158fdff4a3317643d98be97576fe
fazal <fazal@advancedwebtechnologies.com>
2021-01-11 17:47:35 -0400

    added the images

---
+++ copy-files.js
@@ -0,0 +1,38 @@
+const fs = require('fs').promises
+const copy = require('recursive-copy')
+
+const plugins = [
+  'jquery',
+  'bootstrap'
+]
+const distPlugins = './dist/plugins'
+
+fs.rmdir(distPlugins, { recursive: true })
+  .then(() => {
+    console.log('Plugins directory removed!')
+
+    plugins.forEach(plugin => {
+      copy(`./node_modules/${plugin}`, `${distPlugins}/${plugin}`, function(error, results) {
+        if (error) {
+          console.error(`Copy failed: ${error}`)
+        } else {
+          console.info(`Copied ${results.length} files`)
+        }
+      })
+    })
+  })
+
+const distImages = './dist/images'
+
+fs.rmdir(distImages, { recursive: true })
+  .then(() => {
+    console.log('Images directory removed!')
+
+    copy('./src/images', distImages, function(error, results) {
+      if (error) {
+        console.error(`Copy failed: ${error}`)
+      } else {
+        console.info(`Copied ${results.length} files`)
+      }
+    })
+  })
